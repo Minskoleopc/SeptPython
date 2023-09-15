18657791006d843b5ecbd0ecb496391e25b26551
unknown <Minskoleopc>
2023-09-15 06:06:38 -0700

    adde

--- day13.py
+++ day13.py
@@ -89,6 +89,93 @@ for key in dictB:
     print(key,dictB[key])
 
 
+dictB =  {
+
+    "firstName":"chinmay",
+    "lastName":"deshpande",
+    "age":11,
+    "rollNo":45
+}
+for key in dictB:
+    print(key,dictB[key])
+
+
+vehicle = {
+    "color":"red",
+    "regNo":123
+}
+
+# vehicleB = vehicle
+# vehicleB['color'] = "blue"
+#
+# print(vehicleB)
+# print(vehicle)
+
+#copy()
+vehicleC = vehicle.copy()
+print(vehicleC)
+print(vehicle)
+vehicleC['color'] = "purple"
+print(vehicleC)
+print(vehicle)
+
+
+
+vehicle = {
+    "color":"red",
+    "regNo":123,
+    "city":"pune"
+}
+
+#keys()
+for x in vehicle.keys():
+    print(x)
+
+# values()
+for x in vehicle.values():
+    print(x)
+
+
+# items()
+for x,y in vehicle.items():
+    print(x,y)
+
+
+#clear()
+vehicle.clear()
+print(vehicle)
+
+
+student = {
+    "marks":[11,22,33,4],
+    "school":"DPS",
+    "language":"English"
+}
+
+# print(student)
+# # popitem()
+# student.popitem()
+#
+# #pop()
+# student.pop("school")
+# print(student)
+
+student.update({"age":25})
+print(student)
+
+q1 = student.get("age")
+print(q1)
+
+
+
+
+
+
+
+
+
+
+
 
 
 
