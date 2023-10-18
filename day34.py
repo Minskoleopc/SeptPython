c1f072d2e78b44ac35cd3fea0155b55d869e9063
unknown <Minskoleopc>
2023-10-18 05:57:04 -0700

    added

---
+++ day34.py
@@ -0,0 +1,104 @@
+
+
+
+# compile time
+# program 1
+
+# def addA()
+#     print(9 + 9)
+# addA()
+
+# program 2
+#
+# def addC():
+# print(8 + 9)
+# addC()
+
+
+# program 2
+# run time error
+# print("hello")
+# q1 = int(input('Enter the number one'))
+# q2 = int(input('Enter the number two'))
+# print(q1/q2)
+# print("bye")
+
+# program 3
+#       0  1  2  3
+# lstA = [11,22,33,44]
+# print(lstA[5])
+
+# program 4
+# salary = 15000
+# def incrementedSal(salary):
+#     return salary * 0.10
+# sal = incrementedSal(salary)
+# print(sal)
+
+# program 5
+# Run time error :
+# Exceptions
+
+# Program 6
+# print("hello")
+# try:
+#     q1 = int(input("Enter the number A"))
+#     q2 = int(input("Enter the number B"))
+#     print(q1/q2)
+# except Exception as e:
+#     print(e)
+# print("bye")
+
+# program 7
+# print("hello")
+# try:
+#    lstB = [11,22,33]
+#    print(lstB[3])
+# except Exception as e:
+#     print(e)
+# print("bye")
+
+# program 8
+print("hello")
+try:
+    q1 = int(input("Enter the number A"))
+    q2 = int(input("Enter the number B"))
+    print(q1/q2)
+    lstB = [11,22,33]
+    print(lstB[2])
+    print(q3)
+except ArithmeticError:
+    print("input is not correct")
+except IndexError:
+    print("No many element present")
+except Exception:
+    print("not identified")
+
+
+# try ----- except ----- else ----- finally
+# user defined exception
+# assert error
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# logical error
