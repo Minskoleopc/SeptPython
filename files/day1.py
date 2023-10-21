113ac43d22b6043f5061e1336fa4f34afd9912c7
unknown <Minskoleopc>
2023-10-21 00:08:28 -0700

    added

---
+++ files/day1.py
@@ -0,0 +1,39 @@
+
+# program 1
+# txt handle
+# f = open("mytext5.txt","w")
+# str = input("Enter the text")
+# f.write(str)
+# f.close()
+
+# program 2 read mode
+# f = open('mytext5.txt','r')
+# str2 = f.read()
+# print(str2)
+# f.close()
+
+# program 3 - store multiple names for the string
+# f = open('mytext4.txt','w')
+# print('type @ to stop data writing')
+# userInput = None
+# while userInput != '@' :
+#     userInput = input("Enter the name") # chinmay # sarika @
+#     if userInput != '@':
+#         f.write(userInput + "\n") # chinmay # sarika
+#
+# f.close()
+
+# program 4
+# f = open('mytext4.txt','r')
+# listA = f.readlines()
+# for item in listA:
+#     print(item)
+# f.close()
+
+
+
+
+
+
+
+
